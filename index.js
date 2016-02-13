107ec43ab9eca5f59e04bb8b8db5364f51146898
Chris Lee <chris.leeshanok@gmail.com>
2016-02-13 17:19:55 -0500

    Some more base of the template and app

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const consolidate = require('consolidate');
 const main_router = require('./routes/main');
+const nconf = require('nconf');
+
+//nconf configuration
+nconf.argv().env();
+nconf.file({ file: 'config/config.json' });
+
+//grab config file
+const config = nconf.get('config');
 
 //Instantiate
 const app = express();
@@ -14,7 +22,10 @@ app.set('views', __dirname + '/views');
 //Routing
 app.use(main_router);
 
+//Statics
+app.use('/public', express.static(__dirname + '/public'));
+
 //Start Server
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Listening on port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || config.port, () => {
+    console.log('Listening on port ' + (process.env.PORT || config.port));
 });
--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,14 @@
 "use strict";
 const express = require('express');
 const router = express.Router();
+const nconf = require('nconf');
 
 router.route('/')
 .get((req, res, next) => {
-    res.render('homepage');
+    const config = nconf.get('config');
+    res.render('homepage', {
+        context_root: config.context_root
+    });
 });
 
 module.exports = router;
