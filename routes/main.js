42ff323904e56dd93452c7db2c3223788622519a
Chris Lee <chris.leeshanok@gmail.com>
2016-02-21 16:17:27 -0500

    A bit of cleanup, added no cache param to force requests to ignore cache

--- routes/main.js
+++ routes/main.js
@@ -9,13 +9,17 @@ const router = express.Router();
 const redisClient = redis.createClient();
 
 redisClient.on('connect', () => {
-    console.log('Redis Connected');
+    console.info('Redis Connected');
 });
 
 router.route('/')
 .get((req, res, next) => {
+    //TODO: cleanup after cache implementation
+    const requestTime = new Date();
+    console.info('Request started at ' + requestTime.toString());
     const config = nconf.get('config');
     const blogs = nconf.get('blogs');
+    const CACHING_DISABLED = req.query.hasOwnProperty('nocache');
 
     //Need access to these vars throughout the async process
     let cachedBlogs;
@@ -24,31 +28,41 @@ router.route('/')
 
     async.series({
         getCachedBlogs: (callback) => {
-            //Fetch all the redis cache blogs
-            redisClient.hgetall('blogs', (err, object) => {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    if (!object) {
+            //Get Cached Entries
+            if (CACHING_DISABLED) {
+                console.log('Caching Disabled');
+                cachedBlogs = {};
+                callback(null, {});
+            } else {
+                redisClient.hgetall('blogs', (err, object) => {
+                    if (err) {
+                        console.log(err);
                         cachedBlogs = {};
+                        callback(null, {}); //Just move on without the cache
                     } else {
-                        cachedBlogs = object;
+                        if (!object) {
+                            cachedBlogs = {};
+                        } else {
+                            cachedBlogs = object;
+                        }
                     }
-                }
-                callback(null, object);
-            });
+                    callback(null, object);
+                });
+            }
+
         },
         getFilenames: (callback) => {
-            //If not in cache, add the filename to fetchFiles
+            //Assemble list of uncached filenames to retrieve
             blogs.forEach((blog) => {
                 if (cachedBlogs && cachedBlogs[blog.path]) {
-                    console.log('- Cache hit on ' + blog.path);
+                    console.info('- Cache hit on ' + blog.path);
                 } else {
-                    console.log('- Cache miss on ' + blog.path);
+                    console.info('- Cache miss on ' + blog.path);
                     fetchFiles.push(blog.path);
                 }
             });
             callback(null, fetchFiles);
+
         },
         goToFileSystem: (callback) => {
             //Fetch the missing cached files
@@ -73,14 +87,16 @@ router.route('/')
         //Batched Markdown is guaranteed to return blogs in requested order
         blogs.forEach((blog) => {
             if (cachedBlogs[blog.path]) {
-                console.log('Adding blog ' + blog.path + ' from cache data');
+                console.info('Adding blog ' + blog.path + ' from cache data');
                 blogData.push({blog: cachedBlogs[blog.path]});
             } else {
-                console.log('Adding blog ' + blog.path + ' from fs data');
+                console.info('Adding blog ' + blog.path + ' from fs data');
                 let blogPath = results.getFilenames.shift();
                 let blogHTML = results.goToFileSystem.shift();
 
-                redisClient.hmset('blogs', blogPath, blogHTML); //Cache
+                if (!CACHING_DISABLED) {
+                    redisClient.hmset('blogs', blogPath, blogHTML); //Cache
+                }
 
                 blogData.push({blog: blogHTML});
             }
@@ -91,6 +107,7 @@ router.route('/')
             blog_data: blogData,
             analytics: (process.env.NODE_ENV === 'production')
         });
+        console.info('Request completed in ' + ((new Date()).getTime() - requestTime.getTime()) + 'ms');
     });
 
 
